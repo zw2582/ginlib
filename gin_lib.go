e3c3f3a0c37a4f786deaa7f27a172a1095f694ab
zhouwei <zhou.wei@byxgame.com>
2021-12-23 15:52:27 +0800

    f

--- gin_lib.go
+++ gin_lib.go
@@ -371,7 +371,10 @@ func GracefulExitWeb(engine *gin.Engine, host, port string) {
 	log.Println("got a signal", sig)
 	now := time.Now()
 	cxt, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
-	defer cancel()
+	defer func() {
+		cancel()
+		os.Exit(0)
+	}()
 	err := srv.Shutdown(cxt)
 	if err != nil{
 		log.Println("err", err)
